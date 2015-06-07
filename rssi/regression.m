822f653edd7d1e0a1f9fcf9438406297735b5297
Luis Chavier <lfchavier@circuitar.com.br>
2015-06-07 02:49:42 -0300

    Fix Octave script.

--- rssi/regression.m
+++ rssi/regression.m
@@ -3,10 +3,10 @@
 % Linear regression to find parameters to convert from RSSI to dBm based on MRF24J40 datasheet.
 % The resulting parameters should be used in method getSignalStrength().
 %
-% This is done using the normal equation:
-% Tutorial: http://www.lauradhamilton.com/tutorial-linear-regression-with-octave
+% This is done using the normal equation based on the following tutorial:
+% http://www.lauradhamilton.com/tutorial-linear-regression-with-octave
 
-data = load("rssi.csv");
+data = load('rssi.csv');
 rssi = data(:,1);
 dbm = data(:,2);
 
@@ -14,7 +14,7 @@ X = [ones(length(rssi), 1) rssi];
 theta = (pinv(X'*X))*X'*dbm
 y = rssi * theta(2) + theta(1);
 
-printf("dBm = rssi * %f + %f\n", theta(2), theta(1));
+printf('dBm = rssi * %f + %f\n', theta(2), theta(1));
 
 plot(rssi, dbm, rssi, y);
 xlabel('RSSI Value');
